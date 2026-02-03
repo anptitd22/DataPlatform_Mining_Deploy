51129f5dcc456e28af04a5ba7ba31e265e8cfdb0
anptitd22 <tu123098b@gmail.com>
2026-02-04 02:23:45 +0700

    feat(prefect): add prefect

---
+++ prefect/dag/test_workflow.py
@@ -0,0 +1,19 @@
+from prefect import flow, task
+import datetime
+
+@task(log_prints=True)
+def check_system():
+    # Giả lập một bước kiểm tra hệ thống Lakehouse
+    print(f"--- Kiểm tra hệ thống lúc: {datetime.datetime.now()} ---")
+    return "Ready"
+
+@flow(name="Test-Docker-Mount-Flow", log_prints=True)
+def test_flow():
+    status = check_system()
+    print(f"Trạng thái worker: {status}")
+    print("Nếu bạn thấy dòng này, mount file đã thành công!")
+
+if __name__ == "__main__":
+    test_flow.serve(
+        name="docker-test-deployment"
+    )
